49ca288b7ad02f0e6cefba5b178fe8c3435e2c6e
Luca Davì <luca.davi@studenti.unitn.it>
2023-05-09 17:02:12 +0200

    added basic functionality for request parsing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
-import {Router} from 'express'
+import { json } from 'body-parser';
+import {Router,Request,Response} from 'express'
 //import cors from 'cors';
 
 export const defaultRoute = Router()
@@ -7,6 +8,6 @@ defaultRoute.get('/',(req,res)=>{
     res.send("sono nel router");
 })
 
-defaultRoute.use('/test',(req,res)=>{
-    res.json({test:"successful"})
+defaultRoute.use('/test',(req:Request,res:Response)=>{
+    res.json(req.body)
 })
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,14 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import {defaultRoute} from './routes/routes'
+import bodyParser from 'body-parser';
   
 const app = express();
 dotenv.config();
+
+//bodyparser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
   
 // Handling GET / Request
 app.get('/', (req, res) => {
