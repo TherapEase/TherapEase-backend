f2bcd102822df34b5bfc27203b1a4ff22ad37db8
Luca Davì <luca.davi@studenti.unitn.it>
2023-05-13 13:59:49 +0200

    added token verification, test route for token verification

---
+++ src/controllers/token_checker.ts
@@ -0,0 +1,34 @@
+import {Request,Response,NextFunction} from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+export function tokenCheck(req:Request,res:Response,next:NextFunction) {
+    
+    try{
+        const token = req.body.token || req.query.token || req.headers['x-access-token']
+        if(!token){
+            return res.status(400).json({
+                status:400,
+                successful:false,
+                message:'no token provided'
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+        if(!decoded) return res.status(500).json({
+            status:500,
+            successful:false,
+            message: "Failed token verification"
+        })
+        else{
+            req.body.loggedUser=decoded
+            next()
+        }
+    } catch (error) {
+        return res.status(500).json({
+            status:500,
+            successful:false,
+            message: "Internal Error: " + error
+        })
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { json } from 'body-parser';
 import {Router,Request,Response} from 'express'
 import { login } from '../controllers/controller_utente';
+import { tokenCheck } from '../controllers/token_checker';
 //import cors from 'cors';
 
 export const defaultRoute = Router()
@@ -16,4 +17,13 @@ defaultRoute.use('/test',(req:Request,res:Response)=>{
 defaultRoute.post('/login', async (req:Request,res:Response)=>{
     const result= await login(req,res)
     res.status(result.status).json(result)
+})
+
+defaultRoute.use('/authexample',tokenCheck,(req:Request,res:Response)=>{
+    res.status(200).json({
+        status:200,
+        successful:true,
+        message: "token verification ok",
+        loggedUser: req.body.loggedUser
+    })
 })
