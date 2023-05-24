e1ea2a589de61ce583be340b6c4a1bf56541f69b
achf01 <annachiarafortuna01@gmail.com>
2023-05-24 14:03:02 +0200

    set up prodotti, schema and functions

---
+++ src/controllers/controller_prodotti.ts
@@ -0,0 +1,18 @@
+import { NextFunction } from "express";
+
+
+
+export async function inserisci_prodotto(req:Request,res:Response,next:NextFunction){
+
+
+};
+
+export async function rimuovi_prodotto(req:Request,res:Response,next:NextFunction){
+    
+
+};
+
+export async function get_prodotti(req:Request,res:Response,next:NextFunction){
+    
+
+};
---
+++ src/schemas/prodotto_schema.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose"
+
+export interface IProdotto{
+    nome:string;
+    prezzo: number; //accetta anche numeri decimali
+    n_gettoni: number;
+};
+
+export const schema : Schema= new Schema({
+    nome: {type: String, required: true},
+    prezzo: {type: Number, required:true},
+    n_gettoni: {type:Number, required:true}
+});
+
+export const Prodotto = model<IProdotto>('Prodotto', schema,"Prodotti");
