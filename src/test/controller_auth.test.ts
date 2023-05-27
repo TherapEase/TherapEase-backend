47d964e596956aeee68bbae1738a0cdcf6d37737
Luca Davì <luca.davi@studenti.unitn.it>
2023-05-27 17:57:13 +0200

    added example test with correct login

--- src/test/controller_auth.test.ts
+++ src/test/controller_auth.test.ts
@@ -2,13 +2,36 @@ import request from 'supertest'
 import { app } from '../server'
 import { describe } from 'node:test'
 import { Utente, IUtente } from '../schemas/utente_schema'
+import { FilterQuery } from 'mongoose'
 
 describe('GET /api/v1/', ()=>{
-    test(('POST /login senza password'),async()=>{
 
+    // let UtenteSpy:jest.SpyInstance
+
+    // beforeAll(()=>{
+    //     UtenteSpy = jest.spyOn(Utente,'findOne').mockImplementation((criterias):any=>{
+    //         return {
+    //             _id:"646a5a022d917ee699025609",
+    //             username: 'mario_rossi',
+    //             password: '$2b$08$eZn3ZBFTgjqO9Y.7IKrEOukAj3nsPbec/gMPwWnV2gim.yhVmawSi',
+    //             ruolo:1
+    //         }
+    //     })
+    // })
+    // afterAll(async()=>{
+    //     UtenteSpy.mockRestore()
+    // })
+    test(('POST /login senza password'),async()=>{
         const res = await request(app).post('/api/v1/login').send({
             username:"mario_rossi"
         })
         expect(res.status).toBe(400)
+    })
+    test(('POST /login ok'),async ()=>{
+        const res = await request(app).post('/api/v1/login').send({
+            username:'mario_rossi',
+            password:'abcABC123$'
+        })
+        expect(res.status).toBe(200)
     })
 })
