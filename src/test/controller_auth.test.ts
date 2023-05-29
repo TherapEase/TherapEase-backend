aa3c578fc51f5cea8cb4691585f836fa3eefac40
Luca Davì <luca.davi@studenti.unitn.it>
2023-05-29 15:11:53 +0200

    added registration tests

--- src/test/controller_auth.test.ts
+++ src/test/controller_auth.test.ts
@@ -6,6 +6,18 @@ import { Cliente, ICliente } from '../schemas/cliente_schema'
 import mongoose, { Document, FilterQuery,Query, mongo } from 'mongoose'
 import { Db, MongoClient } from 'mongodb'
 
+        /**
+         * 
+         * Le query vengono eseguite come Utente.findOne(...).exec() ciò significa che:
+         *  findOne deve avere un campo exec
+         *  il campo exec corrisponde ad exec() e ritorna una promise con i valori
+         * 
+         *  quindi bisogna dare una finta implementazione a findOne in cui viene restituito exec
+         *  exec a sua volta viene fintamente implementato restituendo la promise
+         * 
+         */
+
+
 describe('POST /api/v1/registrazione e /registrazione',()=>{
     /**
      * Su <BASE>/registrazione inserisci i dati richiesti, ovvero
@@ -20,35 +32,80 @@ describe('POST /api/v1/registrazione e /registrazione',()=>{
      * 
      *  il provesso termina HTTP 200 OK e viene restituito un token
      */
-    const mario_doc ={
-        username:"mario",
-        password:"abcABC123$$",
-        ruolo:1,
-        nome:"mario",
-        cognome:"rossi",
-        email:"mariorossi@gmail.com",
-        codice_fiscale: "RSSMRA",
-        foto_profilo:"",
-        data_nascita:"2020"
-    }
-    beforeAll(async()=>{
-        mongoose.connect= jest.fn().mockImplementation((conn_string)=>Promise.resolve(true))
-        Utente.create= jest.fn().mockImplementation((doc)=>Promise.resolve(true))
+    let mario_doc:any
+    let giovi_doc:any
+    beforeEach(async()=>{
+        mario_doc ={
+            username:"mario",
+            password:"abcABC123$$",
+            ruolo:1,
+            nome:"mario",
+            cognome:"rossi",
+            email:"mariorossi@gmail.com",
+            codice_fiscale: "RSSMRA",
+            foto_profilo:"",
+            data_nascita:"2020"
+        }
+
+        giovi_doc ={
+            username:"giovi",
+            password:"abcABC123$$",
+            ruolo:2,
+            nome:"Giovanna",
+            cognome:"Bianchi",
+            email:"giovannabianchi@gmail.com",
+            codice_fiscale: "BNCGVN",
+            foto_profilo:"",
+            data_nascita:"2020"
+        }
+
+        mongoose.connect= jest.fn().mockImplementation((conn_string)=>Promise.resolve(true)) //bypass del connect
+        Utente.findOne = jest.fn().mockImplementation(()=>{return{exec:jest.fn().mockResolvedValue(null)}})
+        Utente.create= jest.fn().mockImplementation((doc)=>Promise.resolve(true)) //bypass della create
     })
-    afterAll(()=>{
+    afterEach(()=>{
         jest.restoreAllMocks().clearAllMocks()
     })
 
-    it('POST /registrazione ok',async ()=>{
-        //mario rossi non è registrato: la ricerca torna un valore falsy
-        //mockingoose(Utente).toReturn({},"findOne")
-        //non vogliamo che venga salvato sul db
-        //mockingoose(Utente).toReturn(true,"save")
-        Utente.findOne = jest.fn().mockImplementation(()=>{return{exec:jest.fn().mockResolvedValue(null)}})
+    it('POST /registrazione cliente ok',async ()=>{
         
         const res = await request(app).post('/api/v1/registrazione').send(mario_doc)
         expect(res.status).toBe(200)
         expect(res.body).toHaveProperty("token")
+    })
+
+    it('POST /registrazione terapeuta ok',async()=>{
+        
+        const res = await request(app).post('/api/v1/registrazione').send(giovi_doc)
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveProperty("token")
+    })
+
+    it('POST /registrazione utente con ruolo invalido', async()=>{
+        mario_doc.ruolo=6
+        const res = await request(app).post('/api/v1/registrazione').send(mario_doc)
+        expect(res.status).toBe(403)
+        expect(res.body.successful).toBe(false)
+    })
+    
+    it('POST /registrazione con campi mancanti',async()=>{
+        mario_doc={
+            username: "mario_rossi",
+            password:"abcABC123$",
+            email:"mario.rossi@gmail.com",
+            nome:"mario",
+            cf:"RSSMRA70A01H501S",
+            data_nascita:"01/01/1970"
+        }
+        const res = await request(app).post('/api/v1/registrazione').send(mario_doc)
+        expect(res.status).toBe(400)
+    })
+
+    it('POST /registrazione di un utente già registrato',async()=>{
+        Utente.findOne = jest.fn().mockImplementation(()=>{return{exec:jest.fn().mockResolvedValue(true)}})
+
+        const res = await request(app).post('/api/v1/registrazione').send(mario_doc)
+        expect(res.status).toBe(409)
     })
 
     it(('POST /login senza password'),async()=>{
@@ -58,17 +115,6 @@ describe('POST /api/v1/registrazione e /registrazione',()=>{
         expect(res.status).toBe(400)
     })
     it(('POST /login ok'),async ()=>{
-
-        /**
-         * 
-         * Le query vengono eseguite come Utente.findOne(...).exec() ciò significa che:
-         *  findOne deve avere un campo exec
-         *  il campo exec corrisponde ad exec() e ritorna una promise con i valori
-         * 
-         *  quindi bisogna dare una finta implementazione a findOne in cui viene restituito exec
-         *  exec a sua volta viene fintamente implementato restituendo la promise
-         * 
-         */
         Utente.findOne = jest.fn().mockImplementation(()=>{return{exec:jest.fn().mockResolvedValue({
             _id:123,
             username:"mario_rossi",
