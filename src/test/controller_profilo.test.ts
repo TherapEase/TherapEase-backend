a3b7a17020c3e49dcd6cc5d6a76e314069d3bec3
Luca Davì <luca.davi@studenti.unitn.it>
2023-05-29 17:08:15 +0200

    added modification test

--- src/test/controller_profilo.test.ts
+++ src/test/controller_profilo.test.ts
@@ -13,7 +13,7 @@ describe('test /api/v1/il_mio_profilo /api/v1/il_mio_profilo/modifica /api/v1/pr
     
     let mario_doc:any
     let token = createToken("123","mario_rossi",1)
-
+    //aggiungere anche i controlli per il terapeuta?
     beforeEach(()=>{
         mario_doc={
             username:"mario",
@@ -40,6 +40,28 @@ describe('test /api/v1/il_mio_profilo /api/v1/il_mio_profilo/modifica /api/v1/pr
         Terapeuta.findById = jest.fn().mockImplementation((_id,filter)=>{return{exec:jest.fn().mockResolvedValue(mario_doc)}})
 
         const res = await request(app).get('/api/v1/il_mio_profilo').set("x-access-token",token).send()
+        console.log(res.body.message)
+        expect(res.status).toBe(200)
+    })
+
+    it('POST /api/v1/il_mio_profilo/modifica',async()=>{
+        /**
+         * Su <BASE>/il_mio_profilo/modifica fare una richiesta post contenente
+         *  i campi da modificare e i relativi valori,
+         *  ovvero {
+         * nome:"tomas", 
+         * email:"tom.stel@gmail.com"}
+         */
+        Cliente.findById = jest.fn().mockImplementation((_id,filter)=>{return{exec:jest.fn().mockResolvedValue(mario_doc)}})
+        Terapeuta.findById = jest.fn().mockImplementation((_id,filter)=>{return{exec:jest.fn().mockResolvedValue(mario_doc)}})
+        mario_doc.nome="tomas"
+        mario_doc.email="tom.stel@gmail.com"
+        Cliente.findByIdAndUpdate= jest.fn().mockImplementation((_id,filter)=>{return{exec:jest.fn().mockResolvedValue(mario_doc)}})
+        Terapeuta.findByIdAndUpdate = jest.fn().mockImplementation((_id,filter)=>{return{exec:jest.fn().mockResolvedValue(mario_doc)}})
+        const res = await request(app).post('/api/v1/il_mio_profilo/modifica').set("x-access-token",token).send({
+            nome:"tomas",
+            email:"tom.stel@gmail.com"
+        })
         console.log(res.body.message)
         expect(res.status).toBe(200)
     })
