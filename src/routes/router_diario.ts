637cca5eec2da0436b43ea79a34c63a4ec0f9722
Luca Davì <luca.davi@studenti.unitn.it>
2023-06-07 18:45:16 +0200

    refactoring diario

--- src/routes/router_diario.ts
+++ src/routes/router_diario.ts
@@ -3,22 +3,12 @@ import { tokenCheck } from '../controllers/token_checker'
 import { elimina_pagina, leggi_diario_cliente, leggi_my_diario, modifica_pagina, scrivi_pagina } from '../controllers/controller_diario'
 export const diario_router = Router()
 
-diario_router.post('/crea_pagina', tokenCheck, scrivi_pagina, (req: Request, res: Response) => {
-    res.json(req.body)
-})
+diario_router.post('/crea_pagina', tokenCheck, scrivi_pagina)
 
-diario_router.get('/leggi_pagine', tokenCheck, leggi_my_diario, (req: Request, res: Response) => {
-        res.json(req.body)
-})
+diario_router.get('/leggi_pagine', tokenCheck, leggi_my_diario)
 
-diario_router.get('/leggi_pagine/:id', tokenCheck, leggi_diario_cliente, (req: Request, res: Response) => {
-    res.json(req.body)
-})
+diario_router.get('/leggi_pagine/:id', tokenCheck, leggi_diario_cliente)
 
-diario_router.post('/modifica_pagina', tokenCheck, modifica_pagina, (req: Request, res: Response) => {
-    res.json(req.body)
-})
+diario_router.post('/modifica_pagina', tokenCheck, modifica_pagina)
 
-diario_router.post('/elimina_pagina', tokenCheck, elimina_pagina, (req: Request, res: Response) => {
-    res.json(req.body)
-})
+diario_router.post('/elimina_pagina', tokenCheck, elimina_pagina)
