aa0ba582e746981ea8fc98af574f4d416d4bad86
Luca Davì <luca.davi@studenti.unitn.it>
2023-06-07 19:19:40 +0200

    refactoring recensioni

--- src/controllers/controller_recensione.ts
+++ src/controllers/controller_recensione.ts
@@ -5,116 +5,87 @@ import mongoose from 'mongoose'
 import { Recensione, IRecensione } from '../schemas/recensione_schema'
 
 // metodo per leggere le recensioni del terapeuta di cui si sta visitando il profilo
-export async function read_recensioni(req:Request,res:Response,next:NextFunction) {
+export async function read_recensioni(req:Request,res:Response) {
 
     // controllo ruolo
     if(req.body.loggedUser.ruolo!=1){
-        res.status(403)
-        req.body={
+        res.status(403).json({
             successful:false,
             message:"Invalid role!"
-        }
-        next()
-        return
+        })
     }
 
     try {
         await mongoose.connect(process.env.DB_CONNECTION_STRING)
         const catalogo_recensioni =await Recensione.find({recensito:req.params.id}).exec()
 
-        res.status(200)
-        req.body={
+        res.status(200).json({
             successful:true,
             message:"All reviews retrieved successfully",
             catalogo: catalogo_recensioni
-        }
-        next()
-        return
+        })
     } catch (err) {
-        res.status(500)
-        req.body={
+        res.status(500).json({
             successful:false,
             message:"Server error in review catalog - failed!"+err
-        }
-        next()
-        return
+        })
     }
 }
 
 // metodo attraverso cui un terapeuta legge le sue stesse recensioni
-export async function read_my_recensioni(req:Request,res:Response,next:NextFunction) {
+export async function read_my_recensioni(req:Request,res:Response) {
     
     // controllo ruolo
     if(req.body.loggedUser.ruolo!=2){
-        res.status(403)
-        req.body={
+        res.status(403).json({
             successful:false,
             message:"Invalid role!"
-        }
-        next()
-        return
+        })
     }
-
     try {
         await mongoose.connect(process.env.DB_CONNECTION_STRING)
         const catalogo_recensioni =await Recensione.find({recensito:req.body.loggedUser._id}, {}).exec()
 
-        res.status(200)
-        req.body={
+        res.status(200).json({
             successful:true,
             message:"All reviews retrieved successfully",
             catalogo: catalogo_recensioni
-        }
-        next()
-        return
-        
+        })
     } catch (err) {
-        res.status(500)
-        req.body={
+        res.status(500).json({
             successful:false,
             message:"Server error in review catalog - failed!"
-        }
-        next()
-        return
+        })
     }
 }
 
-export async function scrivi_recensione(req:Request,res:Response,next:NextFunction) {
+export async function scrivi_recensione(req:Request,res:Response) {
 
     // controllo ruolo, imposto cliente
     if(req.body.loggedUser.ruolo!=1){ 
-        res.status(403)
-        req.body={
+        res.status(403).json({
             successful:false,
             message:"Invalid role!"
-        }
-        next()
-        return
+        })
     }
-  
+
     await mongoose.connect(process.env.DB_CONNECTION_STRING)
     let cliente = await Cliente.findById(req.body.loggedUser._id).exec()
 
     if(cliente.associato==""){
-        res.status(409)
-        req.body={
+        res.status(409).json({
             successful:false,
             message:"Client not associated to any therapist!"
-        }
-        next()
-        return
+        })
     }
 
     let terapeuta=await Terapeuta.findById(cliente.associato).exec()   
     // controllo esistenza terapeuta
     if(!terapeuta){  
-        res.status(404)
-        req.body={
+        res.status(404).json({
             successful: false,
             message: "User not found!"
-        }
-        next()
-        return      
+        })
     }
 
     //controllo presenza campi
@@ -124,23 +95,17 @@ export async function scrivi_recensione(req:Request,res:Response,next:NextFuncti
     const data=new Date()
     const recensito=cliente.associato
     if(!voto || !testo) {
-        res.status(400)
-        req.body={
+        res.status(400).json({
             successful: false,
             message: "Not enough arguments!"
-        }
-        next()
-        return
+        })
     }
 
     if(voto<1 || voto>5){
-        res.status(401)
-        req.body={
+        res.status(401).json({
             successful: false,
             message: "Invalid ''voto'', must be <5 and >1!"
-        }
-        next()
-        return
+        })
     }
 
     try{
@@ -148,13 +113,10 @@ export async function scrivi_recensione(req:Request,res:Response,next:NextFuncti
         await mongoose.connect(process.env.DB_CONNECTION_STRING)
         let esistente = await Recensione.findOne({voto:voto, testo:testo, autore:autore, data:data}).exec()
         if(esistente){
-            res.status(409)
-            req.body={
+            res.status(409).json({
                 successful: false,
                 message: "Review already present!"
-            }
-            next()
-            return 
+            })
         }else{
             const schema_recensione= new Recensione<IRecensione>({
                 voto:voto,
@@ -167,21 +129,15 @@ export async function scrivi_recensione(req:Request,res:Response,next:NextFuncti
             await schema_recensione.save();
             await Terapeuta.findByIdAndUpdate({_id:terapeuta._id}, {$push:{recensioni:schema_recensione._id.toString()}}) 
             
-            res.status(200)
-            req.body={
+            res.status(200).json({
                 successful: true,
                 message: "Review successfully inserted!"
-            }
-            next()
-            return
+            })
         }
     }catch(err){
-        res.status(500)
-        req.body={
+        res.status(500).json({
             successful: false,
             message: "Server error in review creation - failed! "
-        }
-        next()
-        return 
+        })
     }
 }
--- src/routes/router_recensioni.ts
+++ src/routes/router_recensioni.ts
@@ -3,14 +3,8 @@ import { tokenCheck } from '../controllers/token_checker'
 import { read_my_recensioni, read_recensioni, scrivi_recensione } from '../controllers/controller_recensione'
 export const recensioni_router = Router()
 
-recensioni_router.post('/recensioni' ,tokenCheck, scrivi_recensione, (req:Request,res:Response)=>{
-    res.json(req.body)
-})
+recensioni_router.post('/recensioni' ,tokenCheck, scrivi_recensione)
 
-recensioni_router.get('/recensioni_associato/:id' ,tokenCheck, read_recensioni, (req:Request,res:Response)=>{
-    res.json(req.body)
-})
+recensioni_router.get('/recensioni_associato/:id' ,tokenCheck, read_recensioni)
 
-recensioni_router.get('/le_mie_recensioni' ,tokenCheck, read_my_recensioni, (req:Request,res:Response)=>{
-    res.json(req.body)
-})
+recensioni_router.get('/le_mie_recensioni' ,tokenCheck, read_my_recensioni)
